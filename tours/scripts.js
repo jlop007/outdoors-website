9381c08d0054dde19dc2af2659d75a2acbc6fa72
dEiC <nightmare.dejan@gmail.com>
2020-08-04 16:28:05 +0200

    Whole Website done

--- tours/scripts.js
+++ tours/scripts.js
@@ -1,1 +1,11 @@
 //start
+//function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+//Open the menu on click
+selectElement('.open').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.add('active');
+})
+//Close the menu on click
+selectElement('.close').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.remove('active');
+})
